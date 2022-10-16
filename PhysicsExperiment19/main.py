a8d67df8ce6451063892c3e87a726850fa42579b
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-10-16 18:03:43 +0800

    add average

--- PhysicsExperiment19/main.py
+++ PhysicsExperiment19/main.py
@@ -1,12 +1,16 @@
 # encoding utf-8
 # physics experiment 19
 
+import numpy as np
+
 x1 = [6.350, 6.350, 6.300, 6.550, 6.545]
 xn = [2.745, 2.790, 2.750, 2.960, 2.950]
 x1_prime = [2.650, 2.710, 2.700, 2.990, 2.980]
 xn_prime = [6.340, 6.365, 6.390, 6.590, 6.575]
 delta_x = [0.0, 0.0, 0.0, 0.0, 0.0]
 n = 5
+lbd = 627.0 * 10 ** (-6)
+foc = 100
 
 
 def main():
@@ -22,6 +26,21 @@ def average():
     print(sum_up_x / 5)
 
 
+def get_a():
+    sum_up_x = 0
+    for i in range(5):
+        sum_up_x += delta_x[i - 1]
+    a = lbd * foc / (sum_up_x / 5)
+    print(a)
+
+
+def get_sigma_delta_x():
+    sigma_delta_x = np.std(delta_x)
+    print(sigma_delta_x)
+
+
 if __name__ == "__main__":
     main()
     average()
+    get_a()
+    get_sigma_delta_x()
