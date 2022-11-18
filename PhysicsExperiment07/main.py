c9177a186b3ba2805fe704d9e6ed04bf584a54b5
CongJyu <87171700+CongJyu@users.noreply.github.com>
2022-11-18 19:31:19 +0800

    Add Physic Experiment 07

--- PhysicsExperiment07/main.py
+++ PhysicsExperiment07/main.py
@@ -44,21 +44,59 @@ def main():
     tab = sum_a_b / 5
     print("A + B 的平均周期是 ", tab)
 
-    # 计算 TA 的方差
-    sigma_a = np.var(A)
-    print("TA 的方差是", sigma_a)
+    # 计算 TA 的标准差
+    sigma_a = np.std(A)
+    print("TA 的标准差是", sigma_a)
 
-    # 计算 T(A+B) 的方差
-    sigma_a_b = np.var(B)
-    print("T(A + B) 的方差是 ", sigma_a_b)
+    # 计算 T(A+B) 的标准差
+    sigma_a_b = np.std(B)
+    print("T(A + B) 的标准差是 ", sigma_a_b)
 
-    # 计算 TA 的平均的方差
+    # 计算 TA 的平均的标准差
     sigma_mean_a = sigma_a / math.sqrt(5)
-    print("TA 的平均方差是 ", sigma_mean_a)
+    print("TA 的平均标准差是 ", sigma_mean_a)
 
-    # 计算 T(A+B) 的平均的方差
+    # 计算 T(A+B) 的平均的标准差
     sigma_mean_a_b = sigma_a_b / math.sqrt(5)
-    print("T(A+B) 的平均方差是 ", sigma_mean_a_b)
+    print("T(A+B) 的平均标准差是 ", sigma_mean_a_b)
+
+    # A 的平均转动惯量
+    j_a = ((ma * g * R * r) / (4 * pi ** 2 * L)) * ta ** 2
+    print("A 的平均转动惯量：", j_a)
+
+    # A + b 的平均转动惯量
+    j_a_b = ((ma + mb) * g * R * r) / (4 * pi ** 2 * L) * tab ** 2
+    print("A + b 的平均转动惯量：", j_a_b)
+
+    # E_j_a 百分比
+    e_j_a = np.sqrt(
+        (2 * sigma_mean_a / ta) ** 2 +
+        (sigma_ma / ma) ** 2 +
+        (sigma_L / L) ** 2 +
+        (sigma_R / R) ** 2 +
+        (sigma_r / r) ** 2
+    ) * 100
+    print("Eja 百分比：", e_j_a, "%")
+
+    # E_j_ab 百分比
+    e_j_ab = np.sqrt(
+        (2 * sigma_mean_a_b / tab) ** 2 +
+        ((sigma_ma ** 2 + sigma_mb ** 2) / (ma + mb) ** 2) +
+        (sigma_L / L) ** 2 +
+        (sigma_R / R) ** 2 +
+        (sigma_r / r) ** 2
+    ) * 100
+    print("Ejab 百分比：", e_j_ab, "%")
+
+    # B 的转动惯量
+    j_b = j_a_b - j_a
+    print("B 的转动惯量：", j_b)
+
+    # 理论值
+    j_a0 = 0.5 * ma * R_a ** 2
+    print("理论值：Ja =", j_a0)
+    j_b0 = 0.5 * (mb * R_b ** 2 + mb * r_b ** 2)
+    print("理论值：Jb0 =", j_b0)
 
 
 if __name__ == "__main__":
